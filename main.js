c12f1ce415a212ba91b8f68b0452bb238610a72e
andreikurnosov <nixdie@yandex.ru>
2022-02-27 12:58:59 +0700

    Created and styled Team section

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 import './style.css'
+
+document.querySelectorAll('.story_btn').forEach((btn) => {
+  btn.addEventListener('click', () => {
+    btn.classList.toggle('change')
+    btn.nextElementSibling.classList.toggle('change')
+  })
+})
